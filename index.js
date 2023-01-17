3b14b6910f3b22b5d2cfad436028ff4d08109335
Francisco <franciscowanuelpereira@gmail.com>
2023-01-17 00:20:26 +0000

    finished exercise

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 // Iteration 1: Names and Input
-
+const hacker1 = "Francisco";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Rafaela";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
 // Iteration 3: Loops
+//3.1
+let driverUpperCase = "";
+for (let i = 0; i < hacker1.length; i++) {
+  driverUpperCase += hacker1[i].toUpperCase() + " ";
+}
+console.log(driverUpperCase);
+//3.2
+let reverseNavigator = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reverseNavigator += hacker2[i];
+}
+console.log(reverseNavigator);
+//3.3
+
+hacker1 = hacker1.toLowerCase();
+hacker2 = hacker2.toLowerCase();
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,47 @@
-console.log("I'm ready!");
+// Iteration 1: Names and Input
+const hacker1 = "Francisco";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Rafaela";
+console.log(`The navigator's name is ${hacker2}`);
+
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
+
+// Iteration 3: Loops
+//3.1
+let driverUpperCase = "";
+for (let i = 0; i < hacker1.length; i++) {
+  driverUpperCase += hacker1[i].toUpperCase() + " ";
+}
+console.log(driverUpperCase);
+//3.2
+let reverseNavigator = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reverseNavigator += hacker2[i];
+}
+console.log(reverseNavigator);
+//3.3
+
+for (let i = 0; i < hacker1.length; i++) {
+    for (let j = 0; j < hacker2.length; j++) {
+        if(i[i] > j[j]) {
+            console.log("The driver's name goes first.");
+        } else if (i[i] < j[j]) {
+            console.log("Yo, the navigator goes first definitely.");
+        } else {
+            console.log("What?! You both have the same name?")
+        }
+    }
+}
